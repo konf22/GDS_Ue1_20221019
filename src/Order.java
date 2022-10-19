226706b1c0c041945fb5d6d47b07509927576990
skochauf <stephan.kochauf@lv.campus02.at>
2022-10-19 20:31:30 +0200

    Finalisierung Bestellverwaltung (mit englischen Namen)

--- src/Order.java
+++ src/Order.java
@@ -7,14 +7,14 @@ public class Order {
         this.arrayOfOrderLines = arrayOfOrderLines;
     }
 
-    public void printBestellung() {
+    public void printOrder() {
         System.out.println("Order has " + arrayOfOrderLines.length + " lines:");
         for (int i = 0; i < arrayOfOrderLines.length; i++) {
             System.out.println(arrayOfOrderLines[i].toString());
         }
     }
 
-    public double getCosts() {
+    public double getTotalCosts() {
         double costs = 0;
 
         for (int i = 0; i < arrayOfOrderLines.length; i++) {
@@ -22,6 +22,23 @@ public class Order {
         }
 
         return costs;
+    }
+
+    public double getAverageOrderlineCosts() {
+        double costs = 0;
+        for (int i = 0; i < arrayOfOrderLines.length; i++) {
+            costs += arrayOfOrderLines[i].getCosts();
+        }
+        return costs / (arrayOfOrderLines.length * 1.0);
+    }
+
+    public double getAverageOrderlineCosts2() {
+        double totalCosts = getTotalCosts();
+        return totalCosts / (arrayOfOrderLines.length * 1.0);
+    }
+
+    public double getAverageOrderlineCosts3() {
+        return getTotalCosts() / (arrayOfOrderLines.length * 1.0);
     }
 
     public String getNumber() {
