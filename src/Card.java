176aaeec5db800aac640c16ffa9b75471af52e69
skochauf <stephan.kochauf@lv.campus02.at>
2022-10-19 19:24:05 +0200

    Implementierung der Klasse card

---
+++ src/Card.java
@@ -0,0 +1,43 @@
+public class Card {
+    private int value;
+    private String colour;
+
+    public Card(int value, String colour) {
+        if (value < 1 || value > 9) {
+            System.out.println("Nicht erlaubter Wert: " + value);
+            return;
+        }
+
+        if (!(colour.equals("blue") || colour.equals("green") || colour.equals("red") || colour.equals("yellow"))){
+            System.out.println("Nicht erlaubte Farbe: " + colour);
+            return;
+        }
+
+        this.colour = colour;
+        this.value = value;
+
+/*      if (colour.equals("blue") || colour.equals("green") || colour.equals("red") || colour.equals("yellow")){
+            this.colour = colour;
+            this.value = value;
+        }
+        else {
+            System.out.println("Nicht erlaubte Farbe: " + colour);
+        }
+ */
+    }
+
+    public String toString() {
+        return "Card x{" +
+                "value=" + value +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,5 +1,11 @@
 public class DemoApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        Card card = new Card(1, "red");
+
+        System.out.println(card);
+
     }
 }
